a868fbda6cd539e6265db29e538fec824326f6c5
Julien C <hoopsinet@hotmail.fr>
2022-10-15 16:06:13 +0200

    Debut du background

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './header/navbar';
 import logo_kasa from '../src/img/logo_kasa.png';
+import banniere from './body/background';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-     <img src={logo_kasa} alt="" /> 
+      <div className="header">
+        <img src={logo_kasa} alt="" /> 
+        <Navbar />
+        <banniere />
+      </div>
+     
     </div>
   );
 }
---
+++ src/body/background.js
@@ -0,0 +1,12 @@
+import react, {useState, useEffect} from 'react'
+import "./background.css"
+
+export default function banniere() {
+    return (
+    <div className="banniere">
+        <p className="banniere_text">
+        Chez vous, partout et ailleurs
+        </p>
+   </div>
+    );
+  }
