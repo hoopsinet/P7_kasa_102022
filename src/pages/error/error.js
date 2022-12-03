9cda63cdc25c1c3c2bd65a3fa4d013aeac287b90
Julien C <hoopsinet@hotmail.fr>
2022-12-03 19:36:53 +0100

    correction d'import de css page error.

--- src/pages/error/error.js
+++ src/pages/error/error.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "../about/about.css";
+import '../error/error.css'
 import '../../common.css';
 import { Link } from "react-router-dom";
 import Header from '../../components/common/header/header';
